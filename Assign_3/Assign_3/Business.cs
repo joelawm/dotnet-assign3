0963b3dd75d5abb40471344ac6b225db8241ef65
KennyPacky <qq398965187@gmail.com>
2020-02-17 18:39:51 -0600

    proven to school and business

--- Assign_3/Assign_3/Business.cs
+++ Assign_3/Assign_3/Business.cs
@@ -13,5 +13,12 @@ namespace Assign_3
         readonly BusinessType type;
         readonly string yearEstablished;
         uint activeRecruitment;
+
+        Business(uint id, uint x, uint y, uint o, string sa, string c, string st, string z, bool fs, 
+            string bName, BusinessType btype, string bYear, uint bActive)
+            : base(id, x, y, o, sa, c, st, z, fs)
+        {
+           
+        }
     }
 }
--- Assign_3/Assign_3/School.cs
+++ Assign_3/Assign_3/School.cs
@@ -7,11 +7,17 @@ using System.Threading.Tasks;
 namespace Assign_3
 {
     enum SchoolType { Elementary, HighSchool, CommunityCollege, University }
-    class School
+    class School : Property
     {
         string name;
         readonly SchoolType type;
         string yearEstablished;
         uint enrolled;
+        School(uint id, uint x, uint y, uint o, string sa, string c, string st, string z, bool fs,
+            string cName, SchoolType ctype, string cYear, uint cEnrolled)
+            : base(id, x, y, o, sa, c, st, z, fs)
+        {
+
+        }
     }
 }
